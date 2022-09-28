2d2267d6b496a46fde2b37f575ed5b298758cc81
michellebessa <michelleandressa12@gmail.com>
2022-09-28 20:20:50 +0000

    project updated

--- src/js/app.js
+++ src/js/app.js
@@ -1,49 +1,35 @@
 import "../style/index.css";
 
-/**
- *  EDIT ONLY INSIDE THIS RENDER FUNCTION
- *  This function is called every time the user changes types or changes any input
- * 
-    {
-        includeCover: true, // if includeCover is true the algorithm should
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "left", // social media bar position (left or right)
-        
-        twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
-        instagram: null,
-
-        name: null,
-        lastname: null,
-        role: null,
-        country: null,
-        city: null
-    }
- */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); //print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
+  console.log("These are the current variables: ", variables);
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
-  // reset the website body with the new html output
-  document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name ? variables.name : "First Name"} 
+  document.querySelector(
+    "#widget_content"
+  ).innerHTML = `<div class="widget"> ${cover}<img src="${
+    variables.avatarURL
+  }" class="photo" />
+
+          <h1>${variables.name ? variables.name : "Name"} 
           ${variables.lastname ? variables.lastname : "Last Name"}</h1>
           <h2>${variables.role ? variables.role : "Role"}</h2>
           <h3>${variables.city ? variables.city : "City"} 
-          ${variables.country ? variables.contry : "Country"}</h3>
-          
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter-square"></i>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i>
+          ${variables.country ? variables.country : "Country"}</h3>
+
+          <ul class= "${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${
+              variables.twitter
+            }"><i class="fab fa-twitter-square"></i>
+            <li><a href="https://github.com/${
+              variables.github
+            }"><i class="fab fa-github"></i>
+            <li><a href="https://linkedin.com/${
+              variables.linkedin
+            }"><i class="fab fa-linkedin"></i>
+            <li><a href="https://instagram.com/${
+              variables.instagram
+            }"><i class="fab fa-instagram"></i>
           </ul>
         </div>
     `;
